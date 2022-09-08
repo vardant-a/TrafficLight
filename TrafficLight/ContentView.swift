8504e0f972ffd050a67e3b80de24cd3cdb3fdc9f
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-09-08 10:09:25 +0300

    Initial Commit

---
+++ TrafficLight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLight
+//
+//  Created by Алексей on 08.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
